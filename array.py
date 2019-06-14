306274d1936a5215a35b537a42c5cd73e3fa2cbf
charu11 <charunumesh@gmail.com>
2019-06-14 19:12:13 +0530

    second message

--- array.py
+++ array.py
@@ -1,3 +1,11 @@
 name = ["arsenal", "liverpool", "man utd", "spur"]
 name[3] = "spurs"
-print(name[1:])
+
+
+number = [1,2,3,4,5]
+name.append("man city")
+print(name)
+
+print(number)
+number.insert(2, 5)
+print(number)
---
+++ tuples.py
@@ -0,0 +1,5 @@
+coordinates = (4, 5)
+print(coordinates[0])
+
+# this is tuple. the speciality of tuple is that once the data is added it cant be changed.
+# this is more important when entering the coordinates.
