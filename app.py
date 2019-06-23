57a818a40f5c5a3e34735167cc7bb995e4668a7a
charu11 <charunumesh@gmail.com>
2019-06-23 23:11:10 +0530

    tenth commit

---
+++ app.py
@@ -0,0 +1,6 @@
+from student import Student
+
+student1 = Student("krus", "4th grade", 2.2, False)
+student2 = Student("laca_boom", "3rd grade", 3.3, True)
+print(student2.gpa)
+
---
+++ student.py
@@ -0,0 +1,6 @@
+class Student:
+    def __init__(self, name, grade, gpa, is_probation):
+        self.name = name
+        self.grade = grade
+        self.gpa = gpa
+        self.is_probation = is_probation
